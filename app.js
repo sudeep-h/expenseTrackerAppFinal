f097e18a55e9894dc267f8ce473fac5fcc9b9175
Sudeep4686 <sudeephipparge@gmail.com>
2023-06-06 08:05:38 +0530

    storingUserDetailsInBackend

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const controller = require('./controllers/user');
 const sequelize = require('./util/database');
-const Expense = require('./models/User');
+const axios = require('axios');
+
+const User = require('./models/user');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+sequelize.sync()
+    .then(()=>{
+        console.log("Details synchronised with database")
+    })
+    .catch((err)=>{
+        console.log(err.message);
+        console.log("Failed to sync data with database");
+    })
+
+
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'form.html'));
+    res.sendFile(path.join(__dirname,'routes','user','signup.html'));
 });
+
+app.post('/submit-form', controller.insertData);
 
 app.listen(3100);
