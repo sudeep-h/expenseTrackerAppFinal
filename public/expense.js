3970d200d90ff80579c796798746a8cfa6a81127
Sudeep4686 <sudeephipparge@gmail.com>
2023-06-17 09:05:26 +0530

    finalLeaderboardOPtimised

--- public/expense.js
+++ public/expense.js
@@ -54,26 +54,6 @@ async function addExpense(event) {
     }
 }
 
-
-// function parseJwt (token) {
-//     var base64Url = token.split('.')[1];
-//     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-//     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-//         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-//     }).join(''));
-//     return JSON.parse(jsonPayload);
-// }    
-
-// const token = localStorage.getItem('token');
-// const decodeToken = parseJwt(token);
-// console.log("decodeToken : ",decodeToken);
-// const isAdmin = decodeToken.ispremiumuser;
-
-
-// window.onload = function(){
-//     getExpenses();
-// };
-
 async function getExpenses(){
     try{                                                                   
         const response = await axios.get("http://localhost:3100/expense/getExpense",{
